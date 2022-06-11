7d886363625203e20a9ed8dc69c7c20a842f2ed6
rtavares <ricardotavaresit@gmail.com>
2022-06-11 21:20:17 +0100

    refactoring file

--- src/main.js
+++ src/main.js
@@ -1,14 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
-import VueRouter from 'vue-router';
-import routes from "./routes/index"
-
-Vue.use(VueRouter);
-
-const router = new VueRouter({
-  mode: 'history',
-  routes
-});
+import router from "./routes/index"
 
 
 new Vue({
