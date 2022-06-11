15428064b3ffea1b965c8ec166fc817e22b32ae3
rtavares <ricardotavaresit@gmail.com>
2022-06-11 21:19:56 +0100

    export routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,19 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
 import HomeComponent from "./../components/home/HomeComponent";
 import TasksComponent from "./../components/tasks/TasksComponent";
 
+Vue.use(VueRouter);
 
-export default [
+const routes =  [
   { path: '/', component: HomeComponent, name: 'home' },
   { path: '/tasks', component: TasksComponent, name: 'tasks' }
 ]
+
+
+export default new VueRouter({
+  mode: 'history',
+  routes
+});
+
