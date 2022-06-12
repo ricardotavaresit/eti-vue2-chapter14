6f5330018775185c1b16c1fde34bd8d7bba10710
rtavares <ricardotavaresit@gmail.com>
2022-06-12 12:14:34 +0100

    add store file to Vue intance

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from "./routes/index"
+import router from "./routes/index";
+
+import store from "./store/index";
 
 
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
