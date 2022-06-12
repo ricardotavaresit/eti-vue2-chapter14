6117aeed6132d1d0caae4d6dcc411a9869729632
rtavares <ricardotavaresit@gmail.com>
2022-06-12 12:14:52 +0100

    show tasks from store

--- src/components/tasks/TasksComponent.vue
+++ src/components/tasks/TasksComponent.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
+    <ul>
+      <li v-bind:key="index" v-for="(task, index) in getTasks">
+        {{ task.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -11,6 +16,11 @@ export default {
     return {
       title: "Hi Task",
     };
+  },
+  computed: {
+    getTasks() {
+      return this.$store.state.tasks;
+    },
   },
 };
 </script>
