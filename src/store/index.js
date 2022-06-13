ef50e16461ec5ed33c890f475126b239fe9705a1
rtavares <ricardotavaresit@gmail.com>
2022-06-13 10:14:24 +0100

    create mutation TOOGLE_TASK

--- src/store/index.js
+++ src/store/index.js
@@ -6,15 +6,18 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     tasks: [
-      { name: "Task 1", completed: false },
+      { name: "Task 1", completed: true },
       { name: "Task 2", completed: false },
       { name: "Task 3", completed: false },
       { name: "Task 4", completed: false },
     ]
   },
-  mutations:{
-    ADD_TASK(state, task){
+  mutations: {
+    ADD_TASK(state, task) {
       state.tasks.push(task);
+    },
+    TOOGLE_TASK(state, task) {
+      task.completed = !task.completed
     }
   }
 });
