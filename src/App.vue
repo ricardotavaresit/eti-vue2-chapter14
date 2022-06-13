2df43921765a2bbcea9de05140d52a992defb0f1
rtavares <ricardotavaresit@gmail.com>
2022-06-13 18:40:03 +0100

    create computed proprieties

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
 <template>
   <div id="app">
     <router-link to="/">Home</router-link>
-    <router-link to="/tasks">Tasks</router-link>
+    <router-link to="/tasks"
+      >Tasks ( {{ totalTasks }}/{{ totalTasksNotCompleted }} )</router-link
+    >
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "app",
   data() {
     return {
       msg: "Welcome to Your Vue.js App",
     };
+  },
+  computed: {
+    totalTasksNotCompleted() {
+      return this.$store.getters.tasksNotCompleted.length;
+    },
+    totalTasks() {
+      return this.$store.getters.totalTasks.length;
+    },
   },
 };
 </script>
