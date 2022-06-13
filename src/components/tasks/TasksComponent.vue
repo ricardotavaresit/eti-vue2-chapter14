8259e9d0f0978891e0f46b5bb26c42358c73765b
rtavares <ricardotavaresit@gmail.com>
2022-06-13 18:39:29 +0100

    create task list

--- src/components/tasks/TasksComponent.vue
+++ src/components/tasks/TasksComponent.vue
@@ -8,7 +8,31 @@
         v-for="(task, index) in getSortedTasks"
         v-bind:class="{ completed: task.completed }"
       >
-        {{ task.name }} - {{ task.completed ? "Completed" : "Uncompleted" }}
+        {{ task.name }} - {{ task.completed ? "Completed" : "Incomplete" }}
+        <a href="#" v-on:click.prevent="completedTask(task)">Toogle</a>
+      </li>
+    </ul>
+    <hr />
+    <h3>Incomplete Tasks</h3>
+    <ul>
+      <li
+        v-bind:key="index"
+        v-for="(task, index) in getNotCompletedTasks"
+        v-bind:class="{ completed: task.completed }"
+      >
+        {{ task.name }} - {{ task.completed ? "Completed" : "Incomplete" }}
+        <a href="#" v-on:click.prevent="completedTask(task)">Toogle</a>
+      </li>
+    </ul>
+    <hr />
+    <h3>Completed Tasks</h3>
+    <ul>
+      <li
+        v-bind:key="index"
+        v-for="(task, index) in getCompletedTasks"
+        v-bind:class="{ completed: task.completed }"
+      >
+        {{ task.name }} - {{ task.completed ? "Completed" : "Incomplete" }}
         <a href="#" v-on:click.prevent="completedTask(task)">Toogle</a>
       </li>
     </ul>
@@ -37,6 +61,16 @@ export default {
     getSortedTasks() {
       return this.$store.getters.sortedTasks;
     },
+
+    getNotCompletedTasks() {
+      return this.$store.getters.tasksNotCompleted;
+    },
+
+    getCompletedTasks() {
+      return this.$store.getters.tasksCompleted;
+    },
+
+   
   },
   components: {
     TaskAddComponent,
