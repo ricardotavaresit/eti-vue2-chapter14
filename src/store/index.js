9649de1e7bf2cf072424a76ee0af7aca0b7aceb8
rtavares <ricardotavaresit@gmail.com>
2022-06-13 17:57:31 +0100

    create getter sortedTasks

--- src/store/index.js
+++ src/store/index.js
@@ -7,9 +7,9 @@ export default new Vuex.Store({
   state: {
     tasks: [
       { name: "Task 1", completed: true },
-      { name: "Task 2", completed: false },
-      { name: "Task 3", completed: false },
       { name: "Task 4", completed: false },
+      { name: "Task 3", completed: false },
+      { name: "Task 2", completed: false },
     ]
   },
   mutations: {
@@ -23,9 +23,22 @@ export default new Vuex.Store({
   actions: {
     addTask(context, task) {
 
-      //Ajax
-
       context.commit("ADD_TASK", task);
+    }
+  },
+  getters: {
+    sortedTasks(state) {
+      return state.tasks.sort((a, b) => {
+        if (a.name.toLowerCase() < b.name.toLowerCase()) {
+          return -1;
+        }
+
+        if (a.name.toLowerCase() > b.name.toLowerCase()) {
+          return 1;
+        }
+
+        return 0;
+      })
 
     }
   }
