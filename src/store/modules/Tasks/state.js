7fa2e19ae13c1bd68df5d18dfca167fe21a0a802
rtavares <ricardotavaresit@gmail.com>
2022-06-14 10:07:18 +0100

    refactor with modules

---
+++ src/store/modules/Tasks/state.js
@@ -0,0 +1,8 @@
+export default {
+  tasks: [
+    { name: "Task 1", completed: true },
+    { name: "Task 4", completed: false },
+    { name: "Task 3", completed: false },
+    { name: "Task 2", completed: false },
+  ]
+}
