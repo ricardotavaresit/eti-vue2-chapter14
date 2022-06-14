4f0c376cf4481eb7732e7e2277a1846ab2546eea
rtavares <ricardotavaresit@gmail.com>
2022-06-14 09:51:00 +0100

    import objects

--- src/store/index.js
+++ src/store/index.js
@@ -1,53 +1,15 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import state from "./state";
+import mutations from "./mutations";
+import actions from "./actions";
+import getters from "./getters";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    tasks: [
-      { name: "Task 1", completed: true },
-      { name: "Task 4", completed: false },
-      { name: "Task 3", completed: false },
-      { name: "Task 2", completed: false },
-    ]
-  },
-  mutations: {
-    ADD_TASK(state, task) {
-      state.tasks.push(task);
-    },
-    TOOGLE_TASK(state, task) {
-      task.completed = !task.completed
-    }
-  },
-  actions: {
-    addTask(context, task) {
-
-      context.commit("ADD_TASK", task);
-    }
-  },
-  getters: {
-    sortedTasks(state) {
-      return state.tasks.sort((a, b) => {
-        if (a.name.toLowerCase() < b.name.toLowerCase()) {
-          return -1;
-        }
-
-        if (a.name.toLowerCase() > b.name.toLowerCase()) {
-          return 1;
-        }
-
-        return 0;
-      })
-    },
-    tasksNotCompleted(state) {
-      return state.tasks.filter(item => item.completed == false)
-    },
-    tasksCompleted(state) {
-      return state.tasks.filter(item => item.completed == true)
-    },
-    totalTasks(state) {
-      return state.tasks;
-    }
-  }
+  state,
+  mutations,
+  actions,
+  getters
 });
